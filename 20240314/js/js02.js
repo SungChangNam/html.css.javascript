2974019b70e07dbad738436fd26ef39bf97035db
unknown <a90521@daum.net>
2024-03-15 18:16:42 +0900

    2024/03/15수업

---
+++ 20240314/js/js02.js
@@ -0,0 +1,52 @@
+// var: 자바스크립트의 자료형을 의미하며,문자,숫자형에 모두 사용할수 있다.
+// 문자일경우 :let, 숫자일경우= const 자료형으로 분리하여 선얼할 수있다.
+
+//변수 선언
+var num1;
+var mas1;
+
+//변수에 값을 저상(할당)
+num1 =12345;
+msg1 ="Hello Javascript";
+
+
+
+// 변수의 선언과 초기화를 동시에  했다.
+let num2 = 3.14;
+var msg2 = "안녕하세요. 자바 스크립트 입니다." 
+num2 = num2 +345
+
+var num3 = "100+200";
+var msg3 ="안녕 스크립드";
+msg3 = msg3 + 200;
+
+
+
+//boolean 참 거짓 감을 사용
+// true,false
+var isMan =true;
+
+// null - >  의도 적으로 null을 대입
+// null :값을 저장할 공간은 있는데 현재는 비어있다.
+
+var value = null;
+var value2;
+
+
+
+document.write(value+"<br>" );
+
+document.write(value2+"<br>" );
+
+
+
+document.write("<h1>"+num1+"</h1><br>" );
+document.write(msg1+"<br>" );
+
+document.write("<h1>"+num2+"</h1><br>" );
+document.write(msg2+"<br>" );
+
+document.write("<h1>"+num3+"</h1><br>" );
+document.write(msg3+"<br>" );
+document.write(7);
+
